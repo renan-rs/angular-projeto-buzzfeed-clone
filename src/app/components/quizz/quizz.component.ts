c503fb5f7f0989b978bd68e760076a904de642a9
q-qeh-isso <reresky@hotmail.com>
2022-11-14 23:10:24 -0300

    add project

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json'
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css', './quizz.responsive.component.css']
+})
+export class QuizzComponent implements OnInit {
+
+  title:string = "";
+  question:any;
+  questionSelected:any;
+  answers:string[] = [];
+  answerSelected:string = "";
+  questionIndex:number = 0;
+  questionMaxIndex: number = 0;
+  finished:boolean = false;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.question = quizz_questions.questions;
+      this.questionSelected = this.question[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.question.length;
+      // console.log(this.questionIndex);
+      // console.log(this.questionMaxIndex);
+    }
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value);
+    this.nextStep();
+  }
+
+  async nextStep(){
+    this.questionIndex++;
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.question[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
+    }
+  }
+
+  async checkResult(answers: string[]){
+    const result = answers.reduce((prev, curr, i, arr) => {
+      if(arr.filter(item => item === prev).length >
+         arr.filter(item => item === curr).length){
+
+        return prev;
+      } else {
+        return curr;
+      }
+    });
+    return result;
+  }
+
+
+
+}
